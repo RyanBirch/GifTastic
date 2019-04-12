4f15759f333afcc4419d530a129b37b12cb647ba
Ryan Birch <ryan.birch@gmail.com>
2019-04-12 04:59:57 -0400

    began work on animate function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-let topics = ['dog', 'cat', 'horse', 'chicken']
+let topics = ['dog', 'chicken', 'octopus', 'panda']
 
 function displayButtons() {
   $('#buttons-display').empty()
@@ -23,19 +23,35 @@ $('#add-gif').on('click', function() {
 function displayGifs() {
   let key = 'TUOzKgx9bjMSFVmrVSQ3d619fQIY6iG3'
   let topic = $(this).attr('data-name')
-  let queryURL = `https://api.giphy.com/v1/gifs/search?q=${topic}&api_key=${key}&limit=20`
+  let queryURL = `https://api.giphy.com/v1/gifs/search?q=${topic}&api_key=${key}&limit=10`
 
   $.ajax({
     url: queryURL,
     method: 'GET'
   }).then(function(response) {
     console.log(response)
-    for (let i = 0; i < 20; i++) {
+    let imgAnimatedSrc = []
+    let imgStaticSrc = []
+    for (let i = 0; i < 10; i++) {
       // desktop size
-      $('#gif-display').append(`<img src=${response.data[i].images.original.url}>`)
+      imgAnimatedSrc[i] = response.data[i].images.original.url
+      imgStaticSrc[i] = response.data[i].images.original_still.url
+      $('#gif-display').append(`<img src=${imgStaticSrc[i]} id="gif-img" val="still" data-num=${i}>`)
+      // $('#gif-display').append(`<img src=${response.data[i].images.original.url}>`)
     }
+    $('#gif-container').prepend('<div id="gif-display">')
   })
 }
+
+$('#gif-img').on('click', function() {
+  if ($(this).attr('val') === 'still') {
+    $(this).attr('src', imgAnimatedSrc[data-num])
+    $(this).attr('val', 'animated')
+  } else {
+    $(this).attr('src', imgStaticSrc[data-num])
+    $(this).attr('val', 'still')
+  }
+})
 
 $(document).on('click', '.topic', displayGifs);
 
