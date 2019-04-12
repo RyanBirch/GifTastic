c80e150cba22020474e9b570165ca0a043664989
Ryan Birch <ryan.birch@gmail.com>
2019-04-12 03:12:00 -0400

    got gifs rendering

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,42 @@
-let animals = ['dog', 'cat', 'horse', 'chicken']
+let topics = ['dog', 'cat', 'horse', 'chicken']
 
-function makeButtons() {
+function displayButtons() {
   $('#buttons-display').empty()
-  let animalsLength = animals.length
-  for (let i = 0; i < animalsLength; i++) {
+  let topicsLength = topics.length
+  for (let i = 0; i < topicsLength; i++) {
     let button = $('<button>')
-    button.addClass('animal')
-    button.attr('data-name', animals[i])
-    button.text(animals[i])
+    button.addClass('topic')
+    button.attr('data-name', topics[i])
+    button.text(topics[i])
     $('#buttons-display').append(button)
   }
 }
 
 $('#add-gif').on('click', function() {
   event.preventDefault()
-  let animal = $('#gif-input').val().trim()
-  animals.push(animal)
-  makeButtons()
+  let topic = $('#gif-input').val().trim()
+  topics.push(topic)
+  displayButtons()
   $('#gif-input').val('')
 })
 
 function displayGifs() {
-  
+  let key = 'TUOzKgx9bjMSFVmrVSQ3d619fQIY6iG3'
+  let topic = $(this).attr('data-name')
+  let queryURL = `https://api.giphy.com/v1/gifs/search?q=${topic}&api_key=${key}&limit=20`
+
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).then(function(response) {
+    console.log(response)
+    for (let i = 0; i < 20; i++) {
+      // desktop size
+      $('#gif-display').append(`<img src=${response.data[i].images.original.url}>`)
+    }
+  })
 }
 
-makeButtons()
+$(document).on('click', '.topic', displayGifs);
+
+displayButtons()
