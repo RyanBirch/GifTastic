1d6f5da44259414ac7d8544d6db97ab7cbabfa55
Ryan Birch <ryan.birch@gmail.com>
2019-04-18 01:31:40 -0400

    added favorites

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,8 @@
 let topics = ['dog', 'chicken', 'octopus', 'panda']
+/*
 let imgAnimatedSrc = []
 let imgStaticSrc = []
+*/
 
 function displayButtons() {
   $('#buttons-display').empty()
@@ -34,13 +36,21 @@ function displayGifs() {
     console.log(response)
 
     for (let i = 0; i < 10; i++) {
+      /*
       imgAnimatedSrc[i] = response.data[i].images.fixed_height.url
       imgStaticSrc[i] = response.data[i].images.fixed_height_still.url
+      */
+      // this is added
+      let imgAnimatedSrc = response.data[i].images.fixed_height.url
+      let imgStaticSrc = response.data[i].images.fixed_height_still.url
+      // added code ends here
       let rating = response.data[i].rating
       let figure = $('<figure>')
       let image = $(`<img src=${imgStaticSrc[i]} class="gif-img" data-num=${i}>`)
       image.val('still')
       let ratingCap = $(`<figcaption class="rating">Rating: ${rating}</figcaption>`)
+      let favButton = $(`<button class="fav-button" data-still=${imgStaticSrc[i]} data-animated=${imgAnimatedSrc[i]}>&#9733; Favorite</button>`)
+      ratingCap.append(favButton)
       figure.append(image).append(ratingCap)
       $('#inner-container').append(figure)
     }
@@ -58,7 +68,13 @@ function changeSrc() {
   }
 }
 
+function addToFavs() {
+  let img = $(`<img src=${$(this).attr('data-animated')}>`)
+  $('#favorites-display').append(img)
+}
+
 $(document).on('click', '.topic', displayGifs)
 $(document).on('click', '.gif-img', changeSrc)
+$(document).on('click', '.fav-button', addToFavs)
 
 displayButtons()
